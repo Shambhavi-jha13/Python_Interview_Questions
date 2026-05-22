c6aa9f3d5c0ccfed390a961061089c6960b08fa8
Shambhavi <shambhavijha99@gmail.com>
2026-05-22 12:35:35 +0530

    Day 2 code edit

--- Find_Prime.py
+++ Find_Prime.py
@@ -2,10 +2,10 @@ def Prime_or_Not(num):
     if num < 2:
         return False
     elif num >= 2:
-        for i in range(2, num):
+        for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
                 return False
         return True
 
-print(Prime_or_Not(2))
+print(Prime_or_Not(36))
 # print('Hello')
